e156b91c1dd70cb110a79a2d4107370437850f10
毅伟 邱 <1904760892@qq.com>
2024-11-22 21:08:53 +0800

    提交文件

---
+++ Source/TencentCourse_1/TencentCourse_1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TencentCourse_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TencentCourse_1, "TencentCourse_1" );
